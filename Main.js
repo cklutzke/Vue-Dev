a1d0e5cc8ec09af46c04b2c23325825cfd8d6681
Carl Klutzke <carl@phos.net>
2018-06-13 09:33:39 -0400

    Now getting product data from realistic JSON structure.

--- Main.js
+++ Main.js
@@ -1,77 +1,93 @@
 
-/* TODO: Get data from the server. */
-
+// Computational constants.
 const mmPerInch = 25.4;
 const gramsPerOunce = 28.35;
 
-// vitalsData represents product data from the server.
-const vitalsData = {
-  quantityInStock: 5084,
-  color: 'red',
-  category: 'people',
-  material: 'wood',
-  weightOz: 0.05,
-  heightIn: 0.63,
-  widthIn: 0.61,
-  depthIn: 0.40
-}
-
-// pricingData represents product price data from the server.
-const pricingData = [
-  "1@0.1171",
-  "10@0.0806",
-  "100@0.0644",
-  "1000@0.0384"
-]
-
-// prices is an array of bulk pricing tiers, stored as numbers for calculation.
-var prices = [];
-var lastMin = NaN;
-for (var i = pricingData.length - 1; i >= 0; i--) {
-	var priceData = pricingData[i].split("@");
-    var priceTier = {
-    	min: Number(priceData[0]),
-    	max: lastMin,
-      price: Number(priceData[1])
-    };
-    lastMin = priceTier.min;
-    prices.unshift(priceTier);
-}
-
-// priceTable is structured for the Bulk Pricing table display.
-// Maybe this could be done with Bootstrap custom data rendering instead? https://bootstrap-vue.js.org/docs/components/table#custom-data-rendering
-var priceTable = [];
-for (var i = 0; i < prices.length; i++) {
-  var rangeEnd;
-  if (isNaN(prices[i].max)) {
-    rangeEnd = "+";
-  } else {
-    rangeEnd = "-" + (prices[i].max - 1);
-  }
-  var priceRow = {};
-  priceRow.range = "" + prices[i].min + rangeEnd;
-  priceRow.ea = "$" + prices[i].price + " ea";
-  priceTable.push(priceRow);
+/* TODO: Get productData from the server. */
+// productData represents product data from the server.
+const productData = {
+  "badge" : null,
+  "box" : 0,
+  "category" : "People",
+  "color" : "Red",
+  "date_created" : "2011-06-18 23:44:13",
+  "date_published" : "2017-07-13 21:57:14",
+  "date_updated" : "2018-06-12 20:45:38",
+  "departments" : [
+     "game-pieces"
+  ],
+  "depth" : 0.4,
+  "description" : "These painted wood figurines, available in a variety of colors, make a great alternative to pawns for board games with character.  Each one stands 3/4\" tall by 5/8\" at the base, in the shape of the human body with out-stretched arms.",
+  "discontinued" : 0,
+  "family" : "Wood Meeples",
+  "for_resale" : 1,
+  "height" : 0.63,
+  "id" : "DF0FDE0C-9A04-11E0-AACC-432941C43697",
+  "interior_depth" : 0,
+  "interior_height" : 0,
+  "interior_width" : 0,
+  "keywords" : "carcassonne, rampage",
+  "last_price_change" : "2018-01-24 01:44:12",
+  "low_volume" : 0,
+  "made_on_demand" : 0,
+  "material" : "Wood",
+  "msrp" : 0.4,
+  "msrp_10" : 0.38,
+  "msrp_100" : 0.36,
+  "msrp_1000" : 0.3358,
+  "name" : "Meeple, Red",
+  "number_of_sides" : 0,
+  "object_name" : "Part",
+  "object_type" : "part",
+  "on_sale" : 0,
+  "original_price" : "0.1171",
+  "original_price_10" : "0.0806",
+  "original_price_100" : "0.0644",
+  "original_price_1000" : "0.0384",
+  "photo_size" : "300x300",
+  "preview_uri" : "//s3.amazonaws.com/preview.thegamecrafter.com/85C5BBEE-FB1D-11E0-A7F1-96F09866D12C.png",
+  "price" : "0.1171",
+  "price_10" : "0.0806",
+  "price_100" : "0.0644",
+  "price_1000" : "0.0384",
+  "public" : 1,
+  "quantity" : 6484,
+  "quantity_available_for_games" : 6284,
+  "quantity_available_for_shop" : 5084,
+  "sale_ends" : "2013-01-01 00:00:00",
+  "sale_price" : 0,
+  "shop_uri" : "/parts/meeple-red",
+  "size_in_mm" : 15,
+  "sku_id" : "DF0FE6E0-9A04-11E0-AACC-432941C43697",
+  "supplies" : 0,
+  "uri_part" : "meeple-red",
+  "usable_in_games" : 1,
+  "vintage" : 0,
+  "virtual" : 0,
+  "weight" : 0.0502,
+  "width" : 0.61,
+  "youtube_video_id" : "Hj6N59wZi1I"
 }
 
 var productObj = {
-    name: 'Meeple, Red',
-    image: 'https://s3.amazonaws.com/files.thegamecrafter.com/7f2ccd947321d9af7fefe4e029107fe89588e8fd',
-    /* Youtube URLs must be in this "/embed/" format - see https://stackoverflow.com/questions/9934944 */
-    video: 'https://www.youtube.com/embed/Hj6N59wZi1I',
-    alt: 'Photo of Meeple, Red',
-    /* Vue appears unable to resolve &quot; entities in text, hence the \" characters. */
-    description: 'These painted wood figurines, available in a variety of colors, \
-      make a great alternative to pawns for board games with character.  Each one \
-      stands 3/4\" tall by 5/8\" at the base, in the shape of the human \
-      body with out-stretched arms.',
-    unitPrice: "$" + prices[0].price.toFixed(2),
-    priceTableItems: priceTable,
-    priceTableFields: [
-      {key: 'range', label: 'Bulk Pricing'},
-      {key: 'ea', label: ''}
+    name: productData.name,
+    image: "http:" + productData.preview_uri,
+    alt: "Photo of " + productData.name,
+    description: productData.description,
+    // Youtube URLs must be in this "/embed/" format - see https://stackoverflow.com/questions/9934944
+    video: "https://www.youtube.com/embed/" + productData.youtube_video_id,
+    unitPrice: "$" + Number(productData.price).toFixed(2),
+    priceTableItems: [
+      {range: "1-9", ea: "$" + productData.price + " ea"},
+      {range: "10-99", ea: "$" + productData.price_10 + " ea"},
+      {range: "100-999", ea: "$" + productData.price_100 + " ea"},
+      {range: "1000+", ea: "$" + productData.price_1000 + " ea"}
     ],
-    lastPriceChange: "2018-01-24"
+    priceTableFields: [
+      {key: "range", label: "Bulk Pricing"},
+      {key: "ea", label: ""}
+    ],
+    lastPriceChange: productData.last_price_change.split(" ")[0]
 }
 
 window.app = new Vue({
