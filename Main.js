927915da1d13ed919c752c0783a8c479056704c6
Carl Klutzke <carl@phos.net>
2018-06-19 09:34:55 -0400

    Getting all non-table data from server now.

--- Main.js
+++ Main.js
@@ -1,67 +1,24 @@
 
-// productData represents product data from the server.
+// The "Static TGC" application's API key, created at https://www.thegamecrafter.com/account/apikeys.
+const api_key = "034F04B4-7329-11E8-BA7A-8BFD93A6FE1D";
+const test_user_id = "carl@phos.net";
+const test_password = "TODO"; // TODO: Change my TGC password to something unique while I work on this.
+
+// TODO: Is this the right URI to use for production?
+const URI_prefix = "https://www.thegamecrafter.com";
+
+// TODO: Get all product data from the server and remove the productData constant.
+const partID = "DF0FDE0C-9A04-11E0-AACC-432941C43697";
 const productData = {
-  "badge" : null,
-  "box" : 0,
-  "category" : "People",
-  "color" : "Red",
-  "date_created" : "2011-06-18 23:44:13",
-  "date_published" : "2017-07-13 21:57:14",
-  "date_updated" : "2018-06-12 20:45:38",
-  "departments" : [
-     "game-pieces"
-  ],
   "depth" : 0.4,
-  "description" : "These painted wood figurines, available in a variety of colors, make a great alternative to pawns for board games with character.  Each one stands 3/4\" tall by 5/8\" at the base, in the shape of the human body with out-stretched arms.",
-  "discontinued" : 0,
-  "family" : "Wood Meeples",
-  "for_resale" : 1,
   "height" : 0.63,
   "id" : "DF0FDE0C-9A04-11E0-AACC-432941C43697",
-  "interior_depth" : 0,
-  "interior_height" : 0,
-  "interior_width" : 0,
-  "keywords" : "carcassonne, rampage",
-  "last_price_change" : "2018-01-24 01:44:12",
-  "low_volume" : 0,
-  "made_on_demand" : 0,
-  "material" : "Wood",
-  "msrp" : 0.4,
-  "msrp_10" : 0.38,
-  "msrp_100" : 0.36,
-  "msrp_1000" : 0.3358,
-  "name" : "Meeple, Red",
-  "number_of_sides" : 0,
-  "object_name" : "Part",
-  "object_type" : "part",
-  "on_sale" : 0,
-  "original_price" : "0.1171",
-  "original_price_10" : "0.0806",
-  "original_price_100" : "0.0644",
-  "original_price_1000" : "0.0384",
-  "photo_size" : "300x300",
-  "preview_uri" : "//s3.amazonaws.com/preview.thegamecrafter.com/85C5BBEE-FB1D-11E0-A7F1-96F09866D12C.png",
   "price" : "0.1171",
   "price_10" : "0.0806",
   "price_100" : "0.0644",
   "price_1000" : "0.0384",
-  "public" : 1,
   "quantity" : 6484,
-  "quantity_available_for_games" : 6284,
-  "quantity_available_for_shop" : 5084,
-  "sale_ends" : "2013-01-01 00:00:00",
-  "sale_price" : 0,
-  "shop_uri" : "/parts/meeple-red",
-  "size_in_mm" : 15,
-  "sku_id" : "DF0FE6E0-9A04-11E0-AACC-432941C43697",
-  "supplies" : 0,
-  "uri_part" : "meeple-red",
-  "usable_in_games" : 1,
-  "vintage" : 0,
-  "virtual" : 0,
-  "weight" : 0.0502,
   "width" : 0.61,
-  "youtube_video_id" : "Hj6N59wZi1I"
 }
 
 function ouncesToGrams(ounces) {
@@ -82,9 +39,8 @@ function inchesToMm(inches) {
   }
 }
 
+// TODO: How can I generate these tables AFTER I get the product data from the server?
 let productObj = {
-    description: productData.description,
-    unitPrice: "$" + Number(productData.price).toFixed(2),
     vitalsTableItems: [
       {key: "Quantity in Stock", value1: productData.quantity, value2: ""},
       // TODO: Add Color, Category, and Material. With links to other matching parts.
@@ -112,20 +68,8 @@ let productObj = {
     priceTableFields: [
       {key: "range", label: "Bulk Pricing"},
       {key: "ea", label: ""}
-    ],
-    lastPriceChange: productData.last_price_change.split(" ")[0]
+    ]
 }
-
-// The "Static TGC" application's API key, created at https://www.thegamecrafter.com/account/apikeys.
-const api_key = "034F04B4-7329-11E8-BA7A-8BFD93A6FE1D";
-const test_user_id = "carl@phos.net";
-const test_password = "TODO"; // TODO: Change my TGC password to something unique while I work on this.
-
-// TODO: Is this the right URI to use for production?
-const URI_prefix = "https://www.thegamecrafter.com";
-
-// TODO: Get productData from the server. API call is "GET /api/part/xxx".
-const partID = "DF0FDE0C-9A04-11E0-AACC-432941C43697";
 
 console.log("Creating Vue object now.");
 
